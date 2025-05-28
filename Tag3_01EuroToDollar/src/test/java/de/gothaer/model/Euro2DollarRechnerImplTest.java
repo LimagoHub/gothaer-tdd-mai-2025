0f341a7abf9d147c2d15c8f8856bcf3a07a556a5
Jo Wagner <jowagner@limago.de>
2025-05-28 10:18:58 +0200

    initial commit

--- Tag3_01EuroToDollar/src/test/java/de/gothaer/model/Euro2DollarRechnerImplTest.java
+++ Tag3_01EuroToDollar/src/test/java/de/gothaer/model/Euro2DollarRechnerImplTest.java
@@ -12,7 +12,30 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class Euro2DollarRechnerImplTest {
+    @InjectMocks
+    private Euro2DollarRechnerImpl objectUnderTest;
 
+    @Mock
+    private ExchanceService exchangeserviceMock;
+
+    @Test
+    void calculateEuro2Dollar_RuntimeExceptionInUnderlyingService_throwsExchangeServiceException() {
+
+        when(exchangeserviceMock.getRate("USD")).thenThrow(new ArithmeticException("Test"));
+        ExchangeServiceException ex = assertThrows(ExchangeServiceException.class, () -> objectUnderTest.calculateEuro2Dollar(1.0));
+        assertEquals("Wechselkurs konnte nicht ermittelt werden.", ex.getMessage());
+
+    }
+
+    @Test
+    void calculateEuro2Dollar_HappyDay_ReturnsCorrectDollarValue() {
+
+        when(exchangeserviceMock.getRate("USD")).thenReturn(1.1);
+        var result = objectUnderTest.calculateEuro2Dollar(10.0);
+        assertEquals(11.0, result, 0.001);
+        verify(exchangeserviceMock,times(1)).getRate("USD");
+
+    }
 
 
 }
